467d70dc1946b02a8dccc38a4d1b9db2b7e2fb0c
melinaceleg <melinaceleg@gmail.com>
2022-05-09 02:08:16 -0300

    Alarma agregada

--- Receptor/src/main/java/Controlador/Controlador.java
+++ Receptor/src/main/java/Controlador/Controlador.java
@@ -5,10 +5,13 @@ import Modelo.ServidorTCP;
 import Vista.IVista;
 import Vista.VentanaReceptor;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -17,8 +20,7 @@ public class Controlador implements ActionListener, WindowListener, Observer {
     private IVista vista = null;
     private ServidorTCP receptor;
 
-    public Controlador()
-    {
+    public Controlador() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         this.vista = new VentanaReceptor();
         this.vista.addActionListener(this);
         this.vista.addWindowListener(this);
@@ -35,7 +37,11 @@ public class Controlador implements ActionListener, WindowListener, Observer {
     public void update(Observable o, Object arg)
     {
         MensajeEmergencia mensaje = (MensajeEmergencia)arg;
-        vista.MostrarEmergencia(mensaje.getTipoEmergencia(),mensaje.getFecha(), mensaje.getUbicacion());
+        try {
+            vista.MostrarEmergencia(mensaje.getTipoEmergencia(),mensaje.getFecha(), mensaje.getUbicacion());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- Receptor/src/main/java/Vista/VentanaReceptor.java
+++ Receptor/src/main/java/Vista/VentanaReceptor.java
@@ -1,20 +1,31 @@
 package Vista;
 
+import Modelo.Alarma;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class VentanaReceptor extends JFrame implements IVista {
     private JPanel contentPane;
     private JButton buttonSalir;
     private JTable table1;
     private JScrollPane scrollPane;
+    private Alarma alarma;
 
-    public VentanaReceptor() {
+    public VentanaReceptor() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         setTitle("Solicitud de emergencia");
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+
+        alarma = new Alarma();
 
         IniciarTabla();
 
@@ -31,6 +42,7 @@ public class VentanaReceptor extends JFrame implements IVista {
                 System.exit(0);
                 //dispose();
             }
+
         });
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -51,13 +63,24 @@ public class VentanaReceptor extends JFrame implements IVista {
     }
 
     @Override
-    public void MostrarEmergencia(String tipoEmergencia, String fecha, String ubicacion)
-    {
+    public void MostrarEmergencia(String tipoEmergencia, String fecha, String ubicacion) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
         DefaultTableModel model = (DefaultTableModel) table1.getModel();
         model.addRow(new Object[]{tipoEmergencia, ubicacion, fecha});
+        nuevaAlerta(ubicacion);
+
 
         //table1.getColumn("Accion").setCellRenderer(new ButtonRenderer());
         //table1.getColumn("Accion").setCellEditor(new ButtonEditor(new JCheckBox()));
+    }
+
+
+    public void nuevaAlerta(String mensaje) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        alarma.dispararAlarma();
+        int optionType = JOptionPane.OK_OPTION;
+        JOptionPane.showMessageDialog(null,"Alerta de nueva emergencia en "+mensaje,"Nueva Emeergencia",JOptionPane.WARNING_MESSAGE);
+            alarma.frenarAlarma();
+
+
     }
 
     @Override

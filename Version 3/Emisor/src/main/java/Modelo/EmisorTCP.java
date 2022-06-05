cc2cebb6003684dedc0b763742886419b6062287
melinaceleg <melinaceleg@gmail.com>
2022-06-04 22:49:45 -0300

    Corregido Emisor y Jar

--- Version 3/Emisor/src/main/java/Modelo/EmisorTCP.java
+++ Version 3/Emisor/src/main/java/Modelo/EmisorTCP.java
@@ -39,8 +39,6 @@ public class EmisorTCP extends Observable {
         BufferedReader entrada = null; //leer texto de secuencia de entrada
         PrintWriter salida = null; //crear y escribir archivos
 
-        BufferedReader sc = new BufferedReader( new InputStreamReader(System.in));
-
         try {
                 socketCliente = new Socket();
                 SocketAddress socketAddress = new InetSocketAddress(servidor.getIP(), servidor.getPuerto());
@@ -67,7 +65,6 @@ public class EmisorTCP extends Observable {
 
                 salida.close();
                 entrada.close();
-                sc.close();
                 socketCliente.close();
             } catch (Exception e) {
                 System.out.println("Tiempo de espera agotado para conectar al host");
